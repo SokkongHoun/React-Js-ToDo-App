eb67c96783ad86095b966c7be816c17af9741653
Sokkong Houn <sokkonghoun1@gmail.com>
2024-03-04 22:27:19 +0700

    modifying to do list UI

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,17 +7,18 @@ export function TodoList() {
   function TasksBody(props) {
     return (
       <>
-        <div className="mt-2">
+        <div className="added-task-grp flex mt-2">
           <input
             type="text"
-            className="outline-none text-gray-100 rounded-l px-3 p-1 text-xs"
+            id="price"
+            className="block w-full rounded-l-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
             disabled
             value={props.task}
           />
-          <button className="outline-none p-1 text-xs bg-red-950 text-gray-100 hover:bg-red-700 hover:text-gray-50">
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 shadow">
             {props.del}
           </button>
-          <button className="outline-none rounded-r p-1 text-xs bg-green-800 text-gray-100 hover:bg-green-600 hover:text-gray-50">
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-r-md shadow">
             {props.edit}
           </button>
         </div>
@@ -43,23 +44,34 @@ export function TodoList() {
 
   return (
     <main className="flex items-center flex-col mt-11">
-      <h1 className="font-semibold text-left">To-Do List</h1>
+      <h1 className="font-semibold text-gray-800">To-Do List</h1>
       <div>
-        <input
-          className="input-field outline-none text-white rounded-l px-3 p-1 text-xs placeholder:text-gray-100"
-          type="text"
-          placeholder="Add your task"
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)}
-        />
-        <button
-          className="add-btn outline-none rounded-r p-1 text-xs bg-gray-600 text-gray-100 hover:bg-slate-800 hover:text-gray-50"
-          type="submit"
-          onClick={addTask}
+        <label
+          htmlFor="price"
+          className="block text-sm font-medium leading-6 text-gray-900 mb-2"
         >
-          Add
-        </button>
+          Provide input
+        </label>
+        <div className="handle-add-task">
+          <input
+            type="text"
+            id="price"
+            value={inputValue}
+            className="block w-full rounded-l-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+            placeholder="Add your task"
+            onChange={(event) => setInputValue(event.target.value)}
+          />
+          <button
+            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-r-md shadow"
+            onClick={addTask}
+            type="submit"
+          >
+            Add
+          </button>
+        </div>
       </div>
+
+      {/* This is the tasks container */}
       <div className="flex flex-col mt-2">
         {tasks.map((task) => (
           <TasksBody key={task.id} del="Del" edit="Edit" task={task.task} />
